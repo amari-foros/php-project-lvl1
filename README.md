64a59a990a089cb08c50125b7109cf555552a137
amari-foros <amari-foros@yandex.ru>
2021-11-02 17:45:45 +0300

    step6

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/amari-foros/php-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/amari-foros/php-project-lvl1/actions)
 [![asciicast](https://asciinema.org/a/Xya8vtnVRwCBQsF7c5zCXN6lz.svg)](https://asciinema.org/a/Xya8vtnVRwCBQsF7c5zCXN6lz)
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
+[![asciicast](https://asciinema.org/a/E0eOZSuVIqlaEzkHbS6Rubyje.svg)](https://asciinema.org/a/E0eOZSuVIqlaEzkHbS6Rubyje)
---
+++ src/Engine.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Brain\Games\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function run()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
+
+function engine()
+{
+    foreach ($arrayQuestion as $arrayRand) {
+        line("Question: %s", $arrayRand[0]);
+        $answer = prompt('Your answer ');
+        if ($arrayRand[1] != $answer) {
+            line("'$answer' is wrong answer ;(. Correct answer was '$arrayRand[1]'.");
+            line("Let's try again, %s!", $name);
+            break;
+        } else {
+            line("Correct!");
+        }
+        if (!next($arrayQuestion)) {
+            line("Congratulations, %s!", $name);
+        }
+    }
+}
