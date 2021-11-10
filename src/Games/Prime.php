1b68e26910d27d1976d0c0091658cb5f7bb00cc4
amari-foros <amari-foros@yandex.ru>
2021-11-10 20:02:18 +0300

    debug6

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -10,7 +10,8 @@ function numberIsPrime()
     for ($i = 0; $i < 3; $i++) {
         $rand = mt_rand(2, 19);
         if (
-            $rand == 2 || $rand == 3 || $rand == 5 || $rand == 7 || $rand == 11 || $rand == 13 || $rand == 17 || $rand == 19
+            $rand == 2 || $rand == 3 || $rand == 5 || $rand == 7 || $rand == 11 ||
+            $rand == 13 || $rand == 17 || $rand == 19
         ) {
             $trueAnswer = "yes";
         } else {
